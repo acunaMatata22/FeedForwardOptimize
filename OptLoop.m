b20e29f709c7e0e4c25205299c4c572f0eca73df
acunaMatata22 <44925244+acunaMatata22@users.noreply.github.com>
2023-03-02 13:22:26 -0500

    Update OptLoop.m

--- OptLoop.m
+++ OptLoop.m
@@ -3,7 +3,8 @@ disp('Adding the controller Matlab library to the path.')
 run('AEROPATH.m')
 
 % Setup parameters
-samplePeriod = 1;
+samplePeriod = .125;
+dt = samplePeriod*.001;
 sampleCount = 200;
 axes = [0]; % X (using default names)
 
@@ -18,19 +19,22 @@ A3200MotionHome(handle, 1, axes)
 
 %% SETUP OPTIMIZATION
 Aff = 66.04; % Initialize acceleration gain
-d
-
+totalError = 0;
+step_sizes = [.2 .5 1 2 5 10 20 50];
+speed = 200;
 
 %% TEST LOOP
 
 iteration = 0;
+plot()
 
-while abs(derror) > epsilon_finish
+for i = 1:5
     
-    
+    totalError = 0;
     for i = 1:length(step_sizes)
         step = step_sizes(i);
         step_time = step / speed; % The time before starting error collection
+        start_sample = step_time / dt;
 
         % Run step
         disp('Start collecting the data.')
@@ -39,17 +43,23 @@ while abs(derror) > epsilon_finish
         A3200MotionMoveAbs(handle, 1, axes, step, speed)
         A3200MotionWaitForMotionDone(handle, axes, A3200WaitOption.InPosition, -1);
         
-        
-        disp('Retrieving all 1000 data samples.')
-        collectedData = A3200DataCollectionDataRetrieve(handle, dataCollHandle, 1000);
-        
+        % Collect Data
+        disp('Retrieving all data samples.')
+        collectedData = A3200DataCollectionDataRetrieve(handle, dataCollHandle, sampleCount);
         posData = Converter(collectedData(1,:));
         velData = Converter(collectedData(2,:));
-    end
 
+        % Return to 0 and compile error
+        A3200MotionMoveAbs(handle, 1, axes, 0, speed)
+        stepError = posData(start_sample:end) - step;
+        totalError = [totalError sum(stepError)]; % FIXME
+        A3200MotionWaitForMotionDone(handle, axes, A3200WaitOption.InPosition, -1);
+    end
+    figure(1); hold on;
+    plot(totalError) % FIXME Make animated line
 
 end
-
+hold off;
 % disp('Printing out the collected data.')
 % sampleNumber = 1;
 % while sampleNumber <= sampleCount
@@ -59,7 +69,7 @@ end
 % end
 
 %% Plot Results
-figure(1)
+figure(2)
 subplot(2,1,1)
 plot(posData);
 xlabel("Time (ms)");
@@ -73,4 +83,4 @@ ylabel("Velocity (mm/s)");
 disp('Freeing the resources used by the data collection configuration.')
 A3200DataCollectionConfigFree(dataCollHandle);
 disp('Disconnecting from the A3200')
-A3200Disconnect(handle);;
+A3200Disconnect(handle);
